cebb932616635160561872c6c5ef35bbc68b508f
Deep Akabari <deeppatel4256@gmail.com>
2023-09-01 11:05:32 +0530

    Code Challanges

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,16 +1,23 @@
-﻿// string value1 = " a";
-// string value2 = "A";
-// Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+﻿// Code challenge: write code to display the result of a coin flip
 
-// Console.WriteLine(1 > 2);
-// Console.WriteLine(1 < 2);
-// Console.WriteLine(1 >= 1);
-// Console.WriteLine(1 <= 1);
+// Random coin = new Random();
+// Console.WriteLine((coin.Next(0, 2) == 0) ? "heads" : "tails");
 
-// string pangram = "The quick brown fox jumps over the lazy dog.";
-// Console.WriteLine(pangram.Contains("fox"));
-// Console.WriteLine(pangram.Contains("cow"));
+string? permission = Console.ReadLine();
+int level = 20;
 
-string pangram = "The quick brown fox jumps over the lazy dog.";
-Console.WriteLine(!pangram.Contains("fox"));
-Console.WriteLine(!pangram.Contains("cow"));
+if(permission.Contains("Admin")) {
+    if(level > 55) {
+        Console.WriteLine("Welcome, Super Admin user.");
+    } else {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+} else if(permission.Contains("Manager")) {
+    if(level >= 20) {
+        Console.WriteLine("Contact an admin for access.");
+    } else {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+} else {
+    Console.WriteLine("You do not have sufficient privileges.");
+}
